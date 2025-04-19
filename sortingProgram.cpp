959ab42d124d2749e7d26accfc2a7a024e26e596
Marcin Orszulak <marcinorszulak8@gmail.com>
2025-04-19 15:18:29 +0200

    Sorting added to branch sort

--- sortingProgram.cpp
+++ sortingProgram.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>>
 
 void process(std::vector<int>& data) {
     for (int i = 0; i <= data.size(); ++i) {
@@ -7,9 +8,30 @@ void process(std::vector<int>& data) {
     }
 }
 
+void sort(std::vector<int>& data){
+    int temp{0};
+    bool change{true};
+    while(change)
+    {
+        change = false;
+        for (int i= 0; i < data.size(); i++)
+        {
+            if (data[i] < data[i+1])
+            {
+                temp = data[i];
+                data[i] = data[i+1];
+                data[i+1] = temp;
+                change = true;
+            }
+        }
+    }
+
+}
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
     process(numbers);
+    sort(numbers);
     for (const int& x : numbers) {
         std::cout << x << " ";
     }
